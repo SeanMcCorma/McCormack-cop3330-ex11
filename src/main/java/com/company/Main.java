b52490ea4bde8be4d5977189543aae907b2425e9
Seanfrogs <55803970+Seanfrogs@users.noreply.github.com>
2021-09-10 23:12:43 -0400

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many euros are you exchanging? ");
+        double euro = Double.parseDouble(scan.nextLine());
+        System.out.print("What is the exchange rate? ");
+        double rate = Double.parseDouble(scan.nextLine());
+        double usdc_rounded = (Math.ceil(euro*rate*100))/100;
+        System.out.printf("%.2f euros at an exchange rate of %.4f is \n%.2f U.S. dollars. ", euro, rate, usdc_rounded);
+    }
+}
